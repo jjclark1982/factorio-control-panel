a1ea6f91f1d63e303136a6bf2052ef8e9eaec6f1
Jesse Clark <jjclark1982@gmail.com>
2016-05-20 23:05:39 +1000

    select save file from list when starting server

--- assets/app/server.js
+++ assets/app/server.js
@@ -6,74 +6,134 @@ var bodyParser = require('body-parser');
 var child_process = require('child_process');
 var es = require('event-stream');
 var pug = require('pug');
+var fs = require('fs');
 
-var factorioDir = '/usr/local/factorio';
-var factorioExe = factorioDir+'/bin/x64/factorio';
+var paths = {};
+paths.base = '/usr/local/factorio';
+paths.saves = paths.base+'/saves';
+paths.mods = paths.base+'/mods';
+paths.exe = paths.base+'/bin/x64/factorio';
+
 var runningServer = null;
 
 var app = express();
 
-app.use('/saves', serveIndex(factorioDir+'/saves', {icons: true}));
-app.use('/saves', express.static(factorioDir+'/saves'));
-app.use('/mods', serveIndex(factorioDir+'/mods', {icons: true}));
-app.use('/mods', express.static(factorioDir+'/mods'));
+app.use('/saves', serveIndex(paths.saves, {icons: true}));
+app.use('/saves', express.static(paths.saves));
+app.use('/mods', serveIndex(paths.mods, {icons: true}));
+app.use('/mods', express.static(paths.mods));
+app.use('/static', express.static(__dirname+'/static'));
 var admin = express.Router();
 app.use('/', admin);
 
 admin.get('/', function(req, res, next){
-    var options = {
-        pretty: true,
-        cache: false
-    };
-    adminTemplate = pug.compileFile('./admin.pug', options);
-    context = {runningServer: runningServer};
-    html = adminTemplate(context);
-    res.send(html);
-});
-
-admin.use(bodyParser.urlencoded({extended: false}));
-admin.post('/create-save', function(req, res, next){
-    res.type('html');
-    res.writeContinue();
-    for (var i = 0; i < 20; i++) {
-        res.write("                                                  \n");
-    }
-    res.write('<!DOCTYPE html><html><body><pre>');
-
-    var saveName = req.body.saveName;
-    var child = child_process.spawn(factorioExe, ['--create', saveName]);
-    es.merge([child.stdout, child.stderr])
-    .pipe(es.through(function(text){
-        this.emit('data', text);
-    }, function(end){}))
-    .pipe(res);
-
-    child.on('exit', function(code, signal){
-        if (code != 0) {
-            res.write('<b>Error '+code+"</b>\n");
+    fs.readdir(paths.saves, function(err, files){
+        if (err && err.code != 'ENOENT') {
+            return next(err);
         }
-        res.write('</pre><a href=".">Back to Admin Panel</a>');
-        res.end();
+        files = files || [];
+        var saves = [];
+        for (var i = 0; i < files.length; i++) {
+            var filename = files[i];
+            if (filename[0] == '.') {
+                continue;
+            }
+            var save = {
+                path: '/saves/'+filename,
+                title: filename.replace(/\.zip$/, '')
+            }
+            saves.push(save);
+        }
+        var options = {
+            pretty: true,
+            cache: false
+        };
+        adminTemplate = pug.compileFile('./admin.pug', options);
+        context = {
+            runningServer: runningServer,
+            saves: saves
+        };
+        html = adminTemplate(context);
+        res.send(html);
     });
 });
 
+admin.use(bodyParser.urlencoded({extended: false}));
+
+admin.use(function(req, res, next){
+    res.runCommand = function(cmd, args, env) {
+        res.type('html');
+        res.writeContinue();
+        for (var i = 0; i < 20; i++) {
+            res.write("                                                  \n");
+        }
+        res.write('<!DOCTYPE html><html><body><pre>');
+        res.write('<b>$ '+cmd+' '+args.join(' ')+'\n</b>');
+
+        var child = child_process.spawn(cmd, args, env);
+        es.merge([child.stdout, child.stderr])
+        .pipe(es.through(function(text){
+            this.emit('data', text);
+        }, function(end){}))
+        .pipe(res);
+
+        child.on('exit', function(code, signal){
+            if (code != 0) {
+                res.write('<b>Error '+code+"</b>\n");
+            }
+            res.write('</pre><a href=".">Back to Admin Panel</a>');
+            res.end();
+        });
+        return child;
+    };
+    next();
+});
+
+admin.post('/create-save', function(req, res, next){
+    var saveName = req.body.saveName;
+    res.runCommand(paths.exe, ['--create', saveName]);
+});
+
 admin.post('/start-server', function(req, res, next){
-    var saveName = req.body.saveName;    
+    var saveName = req.body.saveName;
     if (runningServer != null) {
         res.send("sorry, server is already running");
     }
     else {
-        runningServer = child_process.spawn(factorioExe, ['--start-server', saveName]);
+        var supportedArgs = {
+            saveName: '--start-server',
+            latencyMS: '--latency-ms',
+            autosaveInterval: '--autosave-interval',
+            autosaveSlots: '--autosave-slots'
+        }
+        var supportedFlags = {
+            disallowCommands: '--disallow-commands',
+            peerToPeer: '--peer-to-peer',
+            noAutoPause: '--no-auto-pause'
+        }
+
+        var args = [];
+        for (var i in supportedArgs) {
+            if (req.body[i]) {
+                args.push(supportedArgs[i]);
+                args.push(req.body[i]);
+            }
+        }
+        for (var i in supportedFlags) {
+            if (req.body[i]) {
+                args.push(supportedFlags[i]);
+            }
+        }
+
+        runningServer = res.runCommand(paths.exe, args);
         runningServer.startDate = new Date();
         runningServer.on('exit', function(code, signal){
             runningServer = null;
         });
-        res.send("server started, maybe");
     }
 });
 
 admin.post('/stop-server', function(req, res, next){
-    var saveName = req.body.saveName;    
     if (runningServer == null) {
         res.send("sorry, server is not running");
     }
